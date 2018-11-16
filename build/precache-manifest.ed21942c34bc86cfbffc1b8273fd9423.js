4b02083456859af33c58c975a2acdba2fde63e83
Charlene <Charlene@Charlene-Chens-MacBook-Pro.local>
2018-11-16 10:44:55 -0800

    doneeee

---
+++ build/precache-manifest.ed21942c34bc86cfbffc1b8273fd9423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f84b87c3a4eef286132",
+    "url": "/ps7/static/js/runtime~main.4f84b87c.js"
+  },
+  {
+    "revision": "919a0f2a53673a220a4d",
+    "url": "/ps7/static/js/main.919a0f2a.chunk.js"
+  },
+  {
+    "revision": "23fb759563f36003efd5",
+    "url": "/ps7/static/js/1.23fb7595.chunk.js"
+  },
+  {
+    "revision": "919a0f2a53673a220a4d",
+    "url": "/ps7/static/css/main.aa95d1ef.chunk.css"
+  },
+  {
+    "revision": "23fb759563f36003efd5",
+    "url": "/ps7/static/css/1.e4694ecb.chunk.css"
+  },
+  {
+    "revision": "3d8442acf451e3f9b7e728388ace0d4a",
+    "url": "/ps7/index.html"
+  }
+];

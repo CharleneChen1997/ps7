5d9308515a6b1b63d0258b80ddaf765091847b1f
Charlene <Charlene@Charlene-Chens-MacBook-Pro.local>
2018-11-15 23:09:45 -0800

    Updates

---
+++ precache-manifest.d0b732b37cc266d14703701593ec29de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ababcf75fb75b51230de",
+    "url": "/manjic/ps7/my-app/static/js/runtime~main.ababcf75.js"
+  },
+  {
+    "revision": "d6e19ca37ccaa6e184de",
+    "url": "/manjic/ps7/my-app/static/js/main.d6e19ca3.chunk.js"
+  },
+  {
+    "revision": "3456cd19a7ec00d47298",
+    "url": "/manjic/ps7/my-app/static/js/1.3456cd19.chunk.js"
+  },
+  {
+    "revision": "d6e19ca37ccaa6e184de",
+    "url": "/manjic/ps7/my-app/static/css/main.f0ae5bbe.chunk.css"
+  },
+  {
+    "revision": "3456cd19a7ec00d47298",
+    "url": "/manjic/ps7/my-app/static/css/1.fa355f34.chunk.css"
+  },
+  {
+    "revision": "427361b8bf1a215ebd7592e2b9874191",
+    "url": "/manjic/ps7/my-app/index.html"
+  }
+];
